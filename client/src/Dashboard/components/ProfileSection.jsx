0248159608991d236966ce2f7fec77d7b2362756
OwaisAbdullah644 <anonymoushackerunknown10@gmail.com>
2025-11-02 09:57:02 +0500

    fixed infinite loop in console

--- client/src/Dashboard/components/ProfileSection.jsx
+++ client/src/Dashboard/components/ProfileSection.jsx
@@ -2,7 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
-import toast from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
+
 
 const ProfileSection = () => {
   const [profile, setProfile] = useState({
