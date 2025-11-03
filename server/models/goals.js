f70e0707466756373b1b1fb967b0bf5e52208beb
OwaisAbdullah644 <anonymoushackerunknown10@gmail.com>
2025-11-03 22:00:12 +0500

    Aad

--- server/models/goals.js
+++ server/models/goals.js
@@ -1,10 +1,12 @@
+const mongoose = require('mongoose');
+
 const goalsSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'register', required: true },
-  goalType: { type: String, required: true }, 
+  goalType: { type: String, required: true },
   target: { type: Number, required: true },
   current: { type: Number, default: 0 },
   deadline: { type: Date, required: true },
   notes: { type: String },
 }, { timestamps: true });
 
-const goals_model = mongoose.model('Goals', goalsSchema);
+module.exports = mongoose.model('Goals', goalsSchema);
